58927a20772a08d33b00ee145a92abc92cc89835
Ola <owaniczek.it@gmail.com>
2023-11-21 21:18:25 +0100

    header beginnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { Header } from "./layout/Header"
+
 export const App: React.FC = () => {
 	return (
 		<div>
+			<Header/>
 			<h1>Hello there!</h1>
 		</div>
 	);
---
+++ src/layout/Header.tsx
@@ -0,0 +1,8 @@
+import './header.css'
+export const Header: React.FC = () => {
+	return (
+		<div className="header-container">
+		<h1 className='header-name'>Star Wars Planet</h1>
+		</div>
+	);
+};
